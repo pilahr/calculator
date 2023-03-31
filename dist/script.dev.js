266d317bde34c105d1c8cf45efab373d7e8d1680
Piyaporn <piyapornlahr@gmail.com>
2023-03-31 12:28:57 +0100

    created updateDisplay function

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -7,18 +7,40 @@ var clearButton = document.querySelector("#clear");
 var signButton = document.querySelector("#sign");
 var percentageButton = document.querySelector("#percentage");
 var equalButton = document.querySelector("#equal");
-console.log(operatorButtons); //ADD CLASS TO EQUAL BUTTON
+var display = document.querySelector(".display-screen__text");
+console.log(display); //ADD CLASS TO EQUAL BUTTON
 
 operatorButtons[4].classList.add("equal"); // ---- VARIABLES ----
 
-var number = ""; // ---- FUNCTIONS ----
+var number = "";
+var operator = ""; // ---- FUNCTIONS ----
 
 var handleNumberClick = function handleNumberClick(event) {
   var number = parseInt(event.target.innerText);
   console.log(number);
+  updateDisplay(number);
+};
+
+var handleOperatorClick = function handleOperatorClick(event) {
+  var operator = event.target.innerText;
+  console.log(operator);
+}; // const handlePercentageClick = (event) => {
+//   const number = parseFloat(number / number) * 100;
+//   console.log(number);
+// };
+// ---- UPDATE DISPLAY FUNCTION ----
+
+
+var updateDisplay = function updateDisplay(number) {
+  display.innerText = number;
 }; // ---- NUMBER BUTTON CLICKED ----
 
 
 numberButtons.forEach(function (button) {
   button.addEventListener("click", handleNumberClick);
-});
+}); // ---- OPERATOR BUTTON CLICKED ----
+
+operatorButtons.forEach(function (button) {
+  button.addEventListener("click", handleOperatorClick);
+}); // ---- PERCENTAGE BUTTON CLICKED ---
+// percentageButton.addEventListener("click", handlePercentageClick);
--- script.js
+++ script.js
@@ -5,21 +5,48 @@ const clearButton = document.querySelector("#clear");
 const signButton = document.querySelector("#sign");
 const percentageButton = document.querySelector("#percentage");
 const equalButton = document.querySelector("#equal");
+const display = document.querySelector(".display-screen__text");
 
-console.log(operatorButtons);
+console.log(display);
+
 //ADD CLASS TO EQUAL BUTTON
 operatorButtons[4].classList.add("equal");
 
 // ---- VARIABLES ----
 let number = "";
+let operator = "";
 
 // ---- FUNCTIONS ----
 const handleNumberClick = (event) => {
   const number = parseInt(event.target.innerText);
   console.log(number);
+  updateDisplay(number);
+};
+
+const handleOperatorClick = (event) => {
+  const operator = event.target.innerText;
+  console.log(operator);
+};
+
+// const handlePercentageClick = (event) => {
+//   const number = parseFloat(number / number) * 100;
+//   console.log(number);
+// };
+
+// ---- UPDATE DISPLAY FUNCTION ----
+const updateDisplay = (number) => {
+  display.innerText = number;
 };
 
 // ---- NUMBER BUTTON CLICKED ----
 numberButtons.forEach((button) => {
   button.addEventListener("click", handleNumberClick);
 });
+
+// ---- OPERATOR BUTTON CLICKED ----
+operatorButtons.forEach((button) => {
+  button.addEventListener("click", handleOperatorClick);
+});
+
+// ---- PERCENTAGE BUTTON CLICKED ---
+// percentageButton.addEventListener("click", handlePercentageClick);
